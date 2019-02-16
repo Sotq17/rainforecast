95c14b1773f0c4499f8be87e6448854b664db529
EC2 Default User <ec2-user@ip-172-31-37-158.ap-southeast-1.compute.internal>
2019-02-16 13:58:33 +0000

    first commit

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,26 @@
+# require_relative 'boot'
+
+# require 'rails/all'
+
+# # Require the gems listed in Gemfile, including any gems
+# # you've limited to :test, :development, or :production.
+# Bundler.require(*Rails.groups)
+
+# module Rain
+#   class Application < Rails::Application
+#     # Initialize configuration defaults for originally generated Rails version.
+#     config.load_defaults 5.2
+#     config.time_zone = 'Tokyo'
+#     config.i18n.defult_locale = :ja
+
+#     # Settings in config/environments/* take precedence over those specified here.
+#     # Application configuration can go into files in config/initializers
+#     # -- all .rb files in that directory are automatically loaded after loading
+#     # the framework and any gems in your application.
+#   end
+# end
+
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -12,4 +35,3 @@ module Railnotice
     config.i18n.default_locale = :ja
   end
 end
-
